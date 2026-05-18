a54069364cd74a43076d91c6e207eb26834fb4a5
Hirzee <faruqhirzi698@gmail.com>
2026-05-18 10:56:00 +0700

    menghubungkan class joko ke dalam public seseorang

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -4,3 +4,10 @@ using namespace std;
 class seseorang {
 public:
     virtual void pesan() = 0;
+    // virtual voidpesan(){
+    //  cout<< "Pesan dari seseorang"<<endl;
+    //}
+};
+
+class joko :public seseorang {
+
